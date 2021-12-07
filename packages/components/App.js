1cf4bce0d069eec331522cf7c4487fde550965e1
borgesgfj <borgesgfj@gmail.com>
2021-12-07 17:18:23 -0300

    Stracting StatusError component from InputField.
     - create component StatusError.
     - import component in App.js
     - stylizing component
     - adjust error messenger for invalid positions

--- packages/components/App.js
+++ packages/components/App.js
@@ -2,6 +2,7 @@ import { Grid, Stack, Button, Heading } from "@chakra-ui/react";
 import { useState } from "react/cjs/react.development";
 import InputField from "./InputField";
 import Square from "./Square";
+import StatusError from "./StatusError";
 
 export default function App(props) {
   const [squares, setSquares] = useState(props.squares);
@@ -136,11 +137,13 @@ export default function App(props) {
   ));
   return (
     <Grid templateColumns={{ base: "auto", lg: "1fr 1fr" }} gap="5" my="5rem">
-      <InputField
-        isSintaxValid={!sintaxErr}
-        isPositionValid={!positionErr}
-        calculateCommand={calculateCommand}
-      />
+      <Stack direction="column" w="80%" mx="auto" mt="-1rem"  py="2rem" spacing="5">
+        <InputField calculateCommand={calculateCommand}  mt="auto" />
+        <StatusError
+          isSintaxValid={!sintaxErr}
+          isPositionValid={!positionErr}
+        />
+      </Stack>
       <Stack direction="column" w="80%" mx="auto" py="5rem" spacing="5">
         <Heading as="h3" fontFamily="Roboto" size="lg">
           {" "}
